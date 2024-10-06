5f1446530356dcebd4d1dbc9fbdaca548d1a78bc
Jenks Gibbons <jenksgibbons@gmail.com>
2024-10-06 13:12:49 -0700

    launch config to template

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -367,8 +367,7 @@ resource "aws_launch_template" "ecs_launch_config" {
 resource "aws_autoscaling_group" "ecs_asg" {
   name                 = "ECS-lab-asg"
   vpc_zone_identifier  = [aws_subnet.lab-subnet-public-1.id]
-  #launch_configuration = aws_launch_template.ecs_launch_config.name
-  launch_template = aws_launch_template.ecs_launch_config.name
+  launch_configuration = aws_launch_template.ecs_launch_config.name
   desired_capacity     = 1
   min_size             = 0
   max_size             = 1
